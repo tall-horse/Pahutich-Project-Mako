d7e1f9fe15aa77f3e2f9e042dff36b7a223d8bab
tall-horse <dima.gamedev@pm.me>
2026-07-06 14:37:09 +0200

    added proper distance check for MeleeAttack, thus for all crabs

--- Project Mako/Assets/Scripts/Shooting/MeleeAttack.cs
+++ Project Mako/Assets/Scripts/Shooting/MeleeAttack.cs
@@ -8,15 +8,23 @@ namespace Mako.Shooting
     {
         [SerializeField] private int _attackPower = 8;
         [SerializeField] private Transform _targetTransform;
+        [SerializeField] private float _attackDistanceSquare = 25f;
         public event Action OnReloadAttack;
         public void Initialize(Transform targetTransform)
         {
             _targetTransform = targetTransform;
         }
+        /// <summary>
+        /// Dealing Damage. Called as animator event
+        /// </summary>
         public void DealDamage()
         {
             HealthNamespace.Health health = _targetTransform.gameObject.GetComponent<HealthNamespace.Health>();
             Shields shields = _targetTransform.gameObject.GetComponent<Shields>();
+            //using squared distance instead of Vector3.Distance to avoid costly Mathf.Sqrt and save performance
+            float distanceSquare = (_targetTransform.position - transform.position).sqrMagnitude;
+            if (distanceSquare > _attackDistanceSquare)
+                return;
             if (shields != null)
             {
                 if (shields.GetShieldCapacity() >= 0)
