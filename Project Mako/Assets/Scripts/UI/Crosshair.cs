868b7aed5713d0ee0f7ed21af2c7bb1780b68f3d
tall-horse <dima.gamedev@pm.me>
2026-07-17 15:53:16 +0200

    temporary cursor workaround

--- Project Mako/Assets/Scripts/UI/Crosshair.cs
+++ Project Mako/Assets/Scripts/UI/Crosshair.cs
@@ -1,41 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Mako.State;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
 namespace Mako.UI
 {
-
     public class Crosshair : MonoBehaviour
     {
-        private Image _cursorImage;
-        [SerializeField] private Sprite _menuSprite;
-        [SerializeField] private Sprite _combatSprite;
-
-        private void Awake()
-        {
-            _cursorImage = GetComponent<Image>();
-        }
+        [SerializeField] private bool _isCursorVisible;
 
         void Start()
         {
-            Cursor.visible = false;
+            Cursor.visible = _isCursorVisible;
         }
 
         void Update()
         {
-            if (GameManager.GameIsPaused == false && SceneManager.GetActiveScene().buildIndex >= 1)
-            {
-                _cursorImage.sprite = _combatSprite;
-            }
-
-            if (GameManager.GameIsPaused == true || SceneManager.GetActiveScene().buildIndex == 0)
-            {
-                _cursorImage.sprite = _menuSprite;
-            }
             transform.position = Mouse.current.position.ReadValue();
         }
     }
